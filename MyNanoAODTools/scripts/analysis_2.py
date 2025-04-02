ea717adfece48a946e785a2e820347baa68391bf
Alfredo Castaneda <castaned@cern.ch>
2025-04-03 01:47:09 +0200

    new files

---
+++ MyNanoAODTools/scripts/analysis_2.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import ROOT
+import math
+import itertools
+from array import array
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
+
+
+class LeptonAnalysis(Module):
+    def __init__(self, outputFile):
+        self.minLeptons = 3
+        self.outputFile = outputFile
+        self.histograms = {}
+        self.cutflow = {}
+
+    def beginJob(self):
+        self.outputFile = ROOT.TFile(self.outputFile, "RECREATE")
+        self.histograms["electron_pt"] = ROOT.TH1F("electron_pt", "Electron pT; pT (GeV); Events", 50, 0, 200)
+        self.histograms["muon_pt"] = ROOT.TH1F("muon_pt", "Muon pT; pT (GeV); Events", 50, 0, 200)
+        self.histograms["invariant_mass"] = ROOT.TH1F("invariant_mass", "Invariant Mass of Leading Electrons; Mass (GeV); Events", 50, 0, 200)
+        
+        self.cutflow = {
+            "total_events": 0,
+            #"trigger_pass": 0,
+            "min_leptons_pass": 0,
+            "z_candidate_found_A": 0,
+            "z_candidate_found_B": 0,
+            "z_candidate_found_C": 0,
+            "z_candidate_found_D": 0,
+            "final_events_A": 0,
+            "final_events_B": 0
+        }
+
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("invariant_mass", "F")  # Define new branch
+        self.out.branch("nLeptons", "F")
+        self.out.branch("nev_passA","I")
+        
+        branches = ["Zmass", "Wmass", "Dr_Z", "Sum_pt", "Sum_mass", "nlep"]
+        
+        for channel in ['A', 'B', 'C', 'D']:
+            self.out.branch(f"{channel}_Lep3W_pt", "F")
+            for branch in branches:
+                self.out.branch(f"{channel}_{branch}", "F")
+            for i in range(1, 3):
+                self.out.branch(f"{channel}_Lep{i}Z_pt", "F")
+        
+        
+        
+
+
+        inputTree.SetBranchStatus("event",1)
+        inputTree.SetBranchStatus("Electron_pdgId",1)
+        inputTree.SetBranchStatus("Electron_charge",1)
+        inputTree.SetBranchStatus("Electron_pt",1)
+        inputTree.SetBranchStatus("Electron_eta",1)
+        inputTree.SetBranchStatus("Electron_phi",1)
+        inputTree.SetBranchStatus("Muon_pt",1)
+        inputTree.SetBranchStatus("Muon_eta",1)
+        inputTree.SetBranchStatus("Muon_phi",1)
+        inputTree.SetBranchStatus("Muon_charge",1)
+        inputTree.SetBranchStatus("Muon_pdgId",1)
+        inputTree.SetBranchStatus("Muon_mass",1)
+        inputTree.SetBranchStatus("MET_phi",1)
+        
+        
+
+    def analyze(self, event):
+        
+        self.cutflow["total_events"] += 1
+        
+        electrons = Collection(event, "Electron")
+        muons = Collection(event, "Muon")
+        leptons = list(muons) + list(electrons)
+        
+        met_pt = event.MET_pt 
+        met_phi = event.MET_phi
+        
+        #---------------ELECTRONES minimut cuts
+        good_electrons = sorted([ele for ele in electrons if ele.pt > 50 and abs(ele.eta) < 2.5], key=lambda x: x.pt, reverse=True) 
+                
+        #---------------MUONES minimut cuts
+        good_muons = sorted([mu for mu in muons if mu.pt > 80 and abs(mu.eta) < 2.4], key=lambda x: x.pt, reverse=True)
+        
+        
+        good_leptons = good_electrons + good_muons
+
+        self.out.fillBranch("nLeptons", len(good_leptons)) #number of leptons per event
+
+        if len(good_leptons) >= self.minLeptons: 
+
+            if len(good_electrons) >= 3:  # Canal A
+
+                self.out.fillBranch("nev_passA", 1) #number of leptons per event
+                
+                best_pair, best_mass_Z = self.findBestZCandidate(good_leptons)
+                self.out.fillBranch("A_Zmass", best_mass_Z)
+                return True
+            
+            return True
+        return True      
+        
+#        if len(good_leptons) >= self.minLeptons:   # at least with 3 leptons
+
+    #         #if passed_in_channel:
+    #         #    return False
+                
+    #         passed_in_channel = True
+                
+    #         self.cutflow["z_candidate_found_A"] += 1
+
+#                best_pair, best_mass_Z = self.findBestZCandidate(good_leptons)
+#                if not best_pair:
+#                    return False  # No se encontro un par Z
+#                self.out.fillBranch("A_Zmass", best_mass_Z)
+#              return True
+#            return True
+#       return True          
+    #         lepton1, lepton2 = best_pair
+    #         lepton1_pt = lepton1.pt
+    #         lepton2_pt = lepton2.pt
+
+    #         leptons = [lepton for lepton in good_leptons if lepton != lepton1 and lepton != lepton2]
+
+    #         best_lep, best_mass_W = self.findBestWCandidate(leptons, met_pt, met_phi)
+    #         if not best_lep:
+    #             return False #no se encontro un lepton para W
+            
+    #         lepton3 = best_lep
+    #         lepton3_pt = lepton3.pt
+
+    #         dr = self.dr_l1l2_Z(best_pair)
+    #         total_pt = lepton1_pt + lepton2_pt + lepton3_pt
+    #         total_mass = best_mass_Z + best_mass_W
+    #         nlep = len(good_leptons)
+
+    #     # Almacenar las ramas para el canal A
+    #          self.out.fillBranch("A_Zmass", best_mass_Z)
+    #         self.out.fillBranch("A_Wmass", best_mass_W)
+    #         self.out.fillBranch("A_Sum_mass", total_mass)
+    #         self.out.fillBranch("A_Dr_Z", dr)
+    #         self.out.fillBranch("A_Lep1Z_pt", lepton1_pt)
+    #         self.out.fillBranch("A_Lep2Z_pt", lepton2_pt)
+    #         self.out.fillBranch("A_Lep3W_pt", lepton3_pt)
+    #         self.out.fillBranch("A_Sum_pt", total_pt)
+    #         self.out.fillBranch("A_nlep", nlep)
+
+    #         self.cutflow["final_events_A"] += 1
+    #         return True  # Termina el analisis para el canal A
+
+    # # **Canal B**
+    #     elif len(good_electrons) >= 2 and len(good_muons) >= 1:  # Asegurate de que hay al menos 2 electrones y 1 muon
+    #         #if passed_in_channel:
+    #         #    return False
+                
+    #         passed_in_channel = True
+                
+    #         self.cutflow["z_candidate_found_B"] += 1
+
+    #         best_pair, best_mass_Z = self.findBestZCandidate(good_leptons)
+    #         if not best_pair:
+    #             print("No se encontro par Z en Canal B")
+    #             return False  # No se encontro un par Z
+
+    #         lepton1, lepton2 = best_pair
+    #         lepton1_pt = lepton1.pt
+    #         lepton2_pt = lepton2.pt
+
+    #         leptons = [lepton for lepton in good_leptons if lepton != lepton1 and lepton != lepton2]
+
+    #         best_lep, best_mass_W = self.findBestWCandidate(leptons, met_pt, met_phi)
+    #         if not best_lep:
+    #             return False #no se encontro un lepton para W
+    #         lepton3 = best_lep
+    #         lepton3_pt = lepton3.pt
+
+    #         dr = self.dr_l1l2_Z(best_pair)
+    #         total_pt = lepton1_pt + lepton2_pt + lepton3_pt
+    #         total_mass = best_mass_Z + best_mass_W
+    #         nlep = len(good_leptons)
+
+    #     # Almacenar las ramas para el canal B
+    #         self.out.fillBranch("B_Zmass", best_mass_Z)
+    #         self.out.fillBranch("B_Wmass", best_mass_W)
+    #         self.out.fillBranch("B_Sum_mass", total_mass)
+    #         self.out.fillBranch("B_Dr_Z", dr)
+    #         self.out.fillBranch("B_Lep1Z_pt", lepton1_pt)
+    #         self.out.fillBranch("B_Lep2Z_pt", lepton2_pt)
+    #         self.out.fillBranch("B_Lep3W_pt", lepton3_pt)
+    #         self.out.fillBranch("B_Sum_pt", total_pt)
+    #         self.out.fillBranch("B_nlep", nlep)
+
+    #         self.cutflow["final_events_B"] += 1
+    #         return True  # Termina el analisis para el canal B
+
+# Si el evento no pasa para ningun canal, no hacer nada o rechazarlo
+#        print("Evento no paso por Canal A ni B")
+#        return True
+       
+#    return True
+
+    def etaphiplane(self, lepton1, lepton2):
+        dr_etaphi = ()       
+    
+    def computeInvariantMass(self, lepton1, lepton2):
+        #Obtener los 4-vectores de los dos leptones
+        e1, px1, py1, pz1 = self.getLorentzVector(lepton1)
+        e2, px2, py2, pz2 = self.getLorentzVector(lepton2)
+        
+        #Calcular la masa invariante (M^2 = E^2 - p^2)
+        mass2 = (e1 + e2) ** 2 - (px1 + px2) ** 2 - (py1 + py2) ** 2 - (pz1 + pz2) ** 2
+        return math.sqrt(mass2) if mass2 > 0 else 0
+    
+    def getLorentzVector(self, lepton):
+        
+        
+        m_lepton = 0.000511 if abs(lepton.pdgId) == 11 else 0.105  # Electron: 0.511 MeV, Muon: 105 MeV
+        e = math.sqrt(lepton.pt**2 * math.cosh(lepton.eta)**2 + 0.000511**2)  # Electron mass ~0.511 MeV
+        
+        #Componentes del momento
+        px = lepton.pt * math.cos(lepton.phi)
+        py = lepton.pt * math.sin(lepton.phi)
+        pz = lepton.pt * math.sinh(lepton.eta)
+        
+        #Valor absoluto del momento
+        #abs_p = lepton.pt * math.cosh(lepton.eta)
+        return e, px, py, pz
+        
+    def findBestZCandidate(self, good_leptons):
+        Z_MASS = 91.2  # Z boson mass in GeV
+        #Finds the lepton pair with invariant mass closest to the Z boson mass"""
+        best_pair = None
+        best_mass_Z = float("inf")
+        min_diff = float("inf")
+
+        for lepton1, lepton2 in itertools.combinations(good_leptons, 2):
+            if lepton1.charge + lepton2.charge != 0:  # Require opposite charge
+                continue
+            if abs(lepton1.pdgId) != abs(lepton2.pdgId): 
+                continue
+            
+            mass = self.computeInvariantMass(lepton1, lepton2)
+            diff = abs(mass - Z_MASS)
+
+            if diff < min_diff:
+                min_diff = diff
+                best_mass_Z = mass
+                best_pair = (lepton1, lepton2)
+                
+        if best_pair:        
+           return best_pair, best_mass_Z  
+        else:
+           return None, 0.0
+           
+    def findBestWCandidate(self, leptons, met_pt, met_phi):
+        W_MASS = 80.4 #W boson mass in GeV
+        best_lep = None
+        min_diff = float("inf")
+        best_mass_W = float("inf")
+        
+        for lepton in leptons:
+            mass = self.WMass(lepton, met_pt, met_phi)
+            diff = abs(mass - W_MASS)
+            
+            if diff < min_diff:
+                min_diff = diff
+                best_mass_W = mass
+                best_lep = lepton
+                
+        if best_lep:
+            return best_lep, best_mass_W
+        else:
+            return None, 0.0
+        
+           
+    def dr_l1l2_Z(self, best_pair):
+        dr_max = 1.5
+        if best_pair:
+           lepton1, lepton2 = best_pair
+           dr = math.sqrt((lepton1.phi - lepton2.phi)**2 + (lepton1.eta - lepton2.eta)**2) 
+           return dr
+        else:
+           return 0
+        
+               
+    def endJob(self):
+        self.outputFile.Write()
+        self.outputFile.Close()
+        
+        print("Cutflow:")
+        for cut, count in self.cutflow.items():
+            print(f"{cut}: {count}")
+        
+    def WMass(self, lepton, met_pt, met_phi):
+        e, px, py, pz = self.getLorentzVector(lepton)
+        
+        #El MET puede tratarse como un neutrino con energia igual a MET_pt
+        # y phi igual a MET_phi
+        met_px = met_pt * math.cos(met_phi)
+        met_py = met_pt * math.sin(met_phi)
+        
+        met_e = met_pt
+        
+        mass2 = (e + met_e) ** 2 - (px + met_px) ** 2 - (py + met_py) ** 2 - (pz) ** 2
+        return math.sqrt(mass2) if mass2 > 0 else 0
+        
+    
+
+
+# Read input file and Condor job ID arguments
+if len(sys.argv) < 4:
+    print("Usage: filterNanoAOD.py <input.root> <cluster_id> <process_id> <out_folder>")
+    sys.exit(1)
+
+inputFile  = sys.argv[1]
+outfolder  = sys.argv[2]
+process    = sys.argv[3]
+outputDir  = f"filteredNanoAOD/{outfolder}/{process}"  # Corrected
+#outputDir = "filteredNanoAOD"
+
+#outputDir = outfolder
+os.makedirs(outputDir, exist_ok=True)
+
+# Use Condor job IDs for unique histogram filenames
+histOutputFile = os.path.join(outputDir, f"histograms_{process}.root")
+branchSelFile = "branchsel.txt"
+
+
+p = PostProcessor(
+    outputDir, [inputFile],
+    cut=None,
+    branchsel=branchSelFile,  # Keeps only selected branches
+    outputbranchsel=None,  # Ensures all new branches are included
+    modules=[LeptonAnalysis(histOutputFile)],
+    noOut=False,
+    justcount=False
+)
+
+p.run()
+
+
+
--- MyNanoAODTools/scripts/cutflow.py
+++ MyNanoAODTools/scripts/cutflow.py
@@ -4,7 +4,7 @@ import ROOT
 ROOT.EnableImplicitMT()
 
 # File and Tree Name (Update if needed)
-file_path = "/eos/user/c/castaned/Wprime_skim/WprimeToWZToWlepZlep_narrow_M600_TuneCP5_13TeV-madgraph-pythia8_RunIIAutumn18NanoAODv6-Nano25Oct2019_102X_upgrade2018_realistic_v20-v1_NANOAODSIM/BB27DD3F-B227-4B49-8AA5-B595818B0E78_Skim.root"  # Change to your actual file
+file_path = "filteredNanoAOD/1/1/BB27DD3F-B227-4B49-8AA5-B595818B0E78_Skim.root"
 tree_name = "Events"
 
 # Load the ROOT file into an RDataFrame
@@ -13,21 +13,23 @@ df = ROOT.RDataFrame(tree_name, file_path)
 # Define the cuts
 MET_CUT = "MET_pt > 40"
 NLEPTON_CUT = "nLeptons >= 3"
-NLEPTON_CUT_A = "A_nlep >= 3"
-MASS_CUT = "(invariant_mass >= 60) && (invariant_mass <= 120)"
+
+CUT_A = "nev_passA == 1"
+CUT_A_MZ = "A_Zmass>70 && A_Zmass<110"
 
 # Initial number of events
 total_events = df.Count().GetValue()
 
 # Apply each cut sequentially
-df_met = df.Filter(MET_CUT)
-events_met = df_met.Count().GetValue()
 
-df_nlepton = df_met.Filter(NLEPTON_CUT)
+df_nlepton = df.Filter(NLEPTON_CUT)
 events_nlepton = df_nlepton.Count().GetValue()
 
-df_nleptonA = df_nlepton.Filter(NLEPTON_CUT_A)
-events_nleptonA = df_nleptonA.Count().GetValue()
+df_nlepton_passA = df_nlepton.Filter(CUT_A)
+events_passA = df_nlepton_passA.Count().GetValue()
+
+df_A_Zmass = df_nlepton_passA.Filter(CUT_A_MZ)
+events_passA_MZ = df_A_Zmass.Count().GetValue()
 
 # Cortes por canal
 
@@ -39,27 +41,27 @@ print("\nCUTFLOW TABLE:")
 print(f"{'Cut':<25}{'Events':>10}")
 print("-" * 35)
 print(f"{'Total Events':<25}{total_events:>10}")
-print(f"{'MET > 40 GeV':<25}{events_met:>10}")
 print(f"{'nLepton ≥ 3':<25}{events_nlepton:>10}")
-print(f"{'nLepton A ≥ 3':<25}{events_nleptonA:>10}")
-#print(f"{'60 ≤ inv_mass ≤ 120':<25}{events_mass:>10}")
+print(f"{'Events in A':<25}{events_passA:>10}")
+print(f"{'Z window':<25}{events_passA_MZ:>10}")
 
-# Optional: Save cutflow to a histogram
-hist = ROOT.TH1F("cutflow", "Cutflow;Cut Stage;Events", 4, 0, 4)
-hist.GetXaxis().SetBinLabel(1, "Total Events")
-hist.GetXaxis().SetBinLabel(2, "MET > 40")
-hist.GetXaxis().SetBinLabel(3, "nLepton ≥ 3")
-#hist.GetXaxis().SetBinLabel(4, "60 ≤ mass ≤ 120")
 
-hist.SetBinContent(1, total_events)
-hist.SetBinContent(2, events_met)
-hist.SetBinContent(3, events_nlepton)
-#hist.SetBinContent(4, events_mass)
+# # Optional: Save cutflow to a histogram
+# hist = ROOT.TH1F("cutflow", "Cutflow;Cut Stage;Events", 4, 0, 4)
+# hist.GetXaxis().SetBinLabel(1, "Total Events")
+# hist.GetXaxis().SetBinLabel(2, "MET > 40")
+# hist.GetXaxis().SetBinLabel(3, "nLepton ≥ 3")
+# #hist.GetXaxis().SetBinLabel(4, "60 ≤ mass ≤ 120")
 
-# Draw and save histogram
-canvas = ROOT.TCanvas("canvas", "Cutflow Histogram", 800, 600)
-hist.Draw()
-canvas.SaveAs("cutflow.png")  # Saves histogram as an image
+# hist.SetBinContent(1, total_events)
+# hist.SetBinContent(2, events_met)
+# hist.SetBinContent(3, events_nlepton)
+# #hist.SetBinContent(4, events_mass)
 
-# Keep the script interactive (if running in a Python script)
-input("Press Enter to exit...")
+# # Draw and save histogram
+# canvas = ROOT.TCanvas("canvas", "Cutflow Histogram", 800, 600)
+# hist.Draw()
+# canvas.SaveAs("cutflow.png")  # Saves histogram as an image
+
+# # Keep the script interactive (if running in a Python script)
+# input("Press Enter to exit...")
